4addf87323dee4815787faed098a04a924a2e4d1
Tushar-Naik <tushar.naik@flipkart.com>
2014-12-31 22:23:56 +0530

    First Commit

---
+++ app/src/main/java/com/example/tusharnaik/devday101app/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.tusharnaik.devday101app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button refresh = (Button) findViewById(R.id.buttonRefresh);
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reloadList(listView);
+            }
+        });
+
+        Button mainPost = (Button) findViewById(R.id.buttonMainPost);
+        mainPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PostMessage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void reloadList(final ListView listView) {
+
+
+        final ArrayList<String> list = new ArrayList<String>();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpClient client = new DefaultHttpClient();
+                String URL = "http://webapi101.azurewebsites.net/Api/Authors";
+                try {
+                    HttpGet httpget = new HttpGet(URL);
+                    HttpResponse response = client.execute(httpget);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+                    StringBuilder builder = new StringBuilder();
+                    for (String line = null; (line = reader.readLine()) != null; ) {
+                        builder.append(line).append("\n");
+                    }
+                    JSONTokener tokener = new JSONTokener(builder.toString());
+                    JSONArray finalResult = new JSONArray(tokener);
+                    System.out.println("finalResult = " + finalResult);
+                    for (int i = 0; i < finalResult.length(); i++) {
+                        JSONObject jsonObject = finalResult.getJSONObject(i);
+                        System.out.println("jsonObject = " + jsonObject);
+                        String name = jsonObject.get("Name").toString();
+                        String posts = jsonObject.get("Posts").toString();
+                        String id = jsonObject.get("Id").toString();
+                        String messageItem = id + ": " + name + "  " + posts;
+                        list.add(messageItem);
+                    }
+                    System.out.println("list = " + list);
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("list = " + list);
+//        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+//                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+//                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+//                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+//                "Android", "iPhone", "WindowsMobile"};
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ app/src/main/java/com/example/tusharnaik/devday101app/PostMessage.java
@@ -0,0 +1,136 @@
+package com.example.tusharnaik.devday101app;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONStringer;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PostMessage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_message);
+        final EditText name = (EditText) findViewById(R.id.editTextName);
+        final EditText id = (EditText) findViewById(R.id.editTextId);
+        final EditText[] message = {(EditText) findViewById(R.id.editTextMessage)};
+        Button post = (Button) findViewById(R.id.buttonPost);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String authorName = name.getText().toString();
+                final String authorId = id.getText().toString();
+                String postMessage = message[0].getText().toString();
+
+                final boolean[] success = new boolean[1];
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String url = "http://webapp101.azurewebsites.net/Home/CreateAuthor";
+                        HttpPost httpPost = new HttpPost(url);
+                        try {
+                            List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+                            pairs.add(new BasicNameValuePair("Id", authorId));
+                            pairs.add(new BasicNameValuePair("Name", authorName));
+                            httpPost.setEntity(new UrlEncodedFormEntity(pairs));
+
+                            DefaultHttpClient httpClient = new DefaultHttpClient();
+
+                            HttpResponse response = httpClient.execute(httpPost);
+                            HttpEntity entity1 = response.getEntity();
+
+                            if (entity1 != null && (response.getStatusLine().getStatusCode() == 201 || response.getStatusLine().getStatusCode() == 200)) {
+                                success[0] = true;
+                            } else {
+                                success[0] = false;
+                            }
+                        } catch (ClientProtocolException e) {
+                            e.printStackTrace();
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(success[0]){
+                    Toast toast = Toast.makeText(PostMessage.this, "Succussfully posted", Toast.LENGTH_SHORT);
+                    toast.show();
+                    finishActivity(0);
+                }
+                else{
+                    Toast toast = Toast.makeText(PostMessage.this, "Error hitting the endpoint: status", Toast.LENGTH_SHORT);
+                    toast.show();
+                    finishActivity(1);
+
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_post_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
